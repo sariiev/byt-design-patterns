f02e908cb0f9e6403b389aaef76cc3a0d293248b
sariiev <s30244@pjwstk.edu.pl>
2025-11-29 11:54:51 +0100

    implement observer pattern

--- src/observer/ConcreteObserver.java
+++ src/observer/ConcreteObserver.java
@@ -1,5 +1,31 @@
 package observer;
 
 public class ConcreteObserver implements Observer {
+    private String username;
+    private Subject subject;
 
+    public ConcreteObserver(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void addSubscriber(Subject subject) {
+        this.subject = subject;
+        subject.registerObserver(this);
+    }
+
+    public void removeSubscriber(Subject subject) {
+        if (this.subject != null && this.subject == subject) {
+            this.subject.removeObserver(this);
+            this.subject = null;
+        }
+    }
+
+    @Override
+    public void update(String availability) {
+        System.out.println(username + ", product is now " + availability);
+    }
 }
